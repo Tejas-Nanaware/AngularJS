cfc1a0775d980c4014364da8807f0fd2b129d03e
Tejas-Nanaware <nanawaret@hotmail.com>
2017-02-27 18:15:28 +0530

    implemented ajax calls

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 var app = angular
-	.module('rozgaar',[]);
+    .module("Rozgaar", [])
+    .config(function ($routeProvider, $locationProvider, $httpProvider) {
+
+    $routeProvider.when('/Home',
+    {
+      templateUrl:    'home.html',
+      controller:     'HomeCtrl'
+    });
+    $routeProvider.when('/MNREGA',
+    {
+      templateUrl:    'MNREGA.html',
+      controller:     'MNREGACtrl'
+    });
+    $routeProvider.when('/Contact',
+    {
+      templateUrl:    'contact.html',
+      controller:     'ContactCtrl'
+    });
+    $routeProvider.otherwise(
+    {
+      redirectTo:     '/Home',
+      controller:     'HomeCtrl', 
+    }
+  );
+});
